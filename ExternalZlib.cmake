246553c8c2ff7a62bf90d5cbaf058421e0ca864c
Erik Smistad <ersmistad@gmail.com>
2025-10-02 10:07:46 +0200

    Use the faster zlib-ng instead of zlib

--- ExternalZlib.cmake
+++ ExternalZlib.cmake
@@ -1,22 +1,25 @@
-# Build zlib
+# Build zlib-ng
 
-create_package_target(zlib 1.2.9)
+create_package_target(zlib 2.2.5)
 if(WIN32)
 create_package_code("
-file(COPY ${SOURCE_DIR}/README DESTINATION ${POST_INSTALL_DIR}/licenses/${NAME}/)
+file(COPY ${SOURCE_DIR}/LICENSE.md DESTINATION ${POST_INSTALL_DIR}/licenses/${NAME}/)
+file(COPY ${SOURCE_DIR}/README.md DESTINATION ${POST_INSTALL_DIR}/licenses/${NAME}/)
 file(COPY ${INSTALL_DIR}/include/ DESTINATION ${POST_INSTALL_DIR}/include/zlib/)
 file(COPY ${INSTALL_DIR}/bin/zlib.dll DESTINATION ${POST_INSTALL_DIR}/bin/)
 file(COPY ${INSTALL_DIR}/lib/zlib.lib DESTINATION ${POST_INSTALL_DIR}/lib/)
 ")
 elseif(APPLE)
 create_package_code("
-file(COPY ${SOURCE_DIR}/README DESTINATION ${POST_INSTALL_DIR}/licenses/${NAME}/)
+file(COPY ${SOURCE_DIR}/LICENSE.md DESTINATION ${POST_INSTALL_DIR}/licenses/${NAME}/)
+file(COPY ${SOURCE_DIR}/README.md DESTINATION ${POST_INSTALL_DIR}/licenses/${NAME}/)
 file(COPY ${INSTALL_DIR}/include/ DESTINATION ${POST_INSTALL_DIR}/include/zlib/)
 file(COPY ${INSTALL_DIR}/lib/libz.dylib DESTINATION ${POST_INSTALL_DIR}/lib/ FOLLOW_SYMLINK_CHAIN)
 ")
 else()
 create_package_code("
-file(COPY ${SOURCE_DIR}/README DESTINATION ${POST_INSTALL_DIR}/licenses/${NAME}/)
+file(COPY ${SOURCE_DIR}/LICENSE.md DESTINATION ${POST_INSTALL_DIR}/licenses/${NAME}/)
+file(COPY ${SOURCE_DIR}/README.md DESTINATION ${POST_INSTALL_DIR}/licenses/${NAME}/)
 file(COPY ${INSTALL_DIR}/include/ DESTINATION ${POST_INSTALL_DIR}/include/zlib/)
 file(COPY ${INSTALL_DIR}/lib/libz.so DESTINATION ${POST_INSTALL_DIR}/lib/ FOLLOW_SYMLINK_CHAIN)
 ")
@@ -25,11 +28,14 @@ endif()
 ExternalProject_Add(${NAME}
 	PREFIX ${BUILD_DIR}
 	BINARY_DIR ${BUILD_DIR}
-        GIT_REPOSITORY "https://github.com/madler/zlib.git"
-	GIT_TAG "v${VERSION}"
+        GIT_REPOSITORY "https://github.com/zlib-ng/zlib-ng.git"
+	GIT_TAG "${VERSION}"
         CMAKE_ARGS
             -DCMAKE_MACOSX_RPATH=ON
 	    -DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=${CMAKE_OSX_DEPLOYMENT_TARGET}
+	    -DZLIB_ENABLE_TESTS=OFF
+	    -DWITH_GTEST=OFF
+	    -DZLIB_COMPAT=ON
         CMAKE_CACHE_ARGS
             -DCMAKE_BUILD_TYPE:STRING=Release
             -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF
